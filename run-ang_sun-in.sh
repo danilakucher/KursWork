132961f2d45e15e143bdd195835f8cee2e83476f
danilakucher <kucherov145@gmail.com>
2025-09-30 13:26:54 +0800

    Применен патч для правки табуляции в файле

--- run-ang_sun-in.sh
+++ run-ang_sun-in.sh
@@ -82,15 +82,15 @@ cd "${bindir}"
 cnt=0
 for ex in $(seq ${fa} $(echo "scale=20 ; fa=${fa} ; fb=${fb} ; nf=${Nf} ; print((fb-fa)/(nf-1))" | bc) ${fb})
 do
-	printf -v datf "%s/${model}_${mod}_${mang}_id%03d.dat" "${tdir}" ${cnt}
-	echo -n "" > "${datf}"
-	for i in $(seq 0 1 $((Ne-1)))
-	do
-		./${prog} ${model_file} -c "Ep1=${Ep1};Ep2=${Ep2};d=(Ep2-Ep1)/(${N}-1);E=math.exp((${Ep1}+${i}*d)*math.log(10));${mang}=${ex}*${mang}" ${prob_file} > "${fdata}"
-		dat=($(grep -v '^#' "${fdata}"))
-		ang=$(grep "#*ex.*${mang}" "${fdata}" | sed -e 's@.*=@@')
-		echo "${dat[0]}  ${dat[1]}  ${dat[2]} ${ang} ${ex}" >> "${datf}"
-	done
+  printf -v datf "%s/${model}_${mod}_${mang}_id%03d.dat" "${tdir}" ${cnt}
+  echo -n "" > "${datf}"
+  for i in $(seq 0 1 $((Ne-1)))
+  do
+    ./${prog} ${model_file} -c "Ep1=${Ep1};Ep2=${Ep2};d=(Ep2-Ep1)/(${N}-1);E=math.exp((${Ep1}+${i}*d)*math.log(10));${mang}=${ex}*${mang}" ${prob_file} > "${fdata}"
+    dat=($(grep -v '^#' "${fdata}"))
+    ang=$(grep "#*ex.*${mang}" "${fdata}" | sed -e 's@.*=@@')
+    echo "${dat[0]}  ${dat[1]}  ${dat[2]} ${ang} ${ex}" >> "${datf}"
+  done
   ((cnt++))
 done
 # vim: ts=2 et tw=120
